8c7a70cbb2e3dab17f7ee2a907cb904e3bda8af1
abirtasrif <abirtasrif@gmail.com>
2022-12-22 23:32:01 +0600

    wip2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./componenets/Home";
+import Navbar from "./componenets/Navbar";
+import Favourites from "./componenets/Favourites";
+import Footer from "./componenets/Footer";
+import NotFound from "./componenets/NotFound";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app min-h-screen">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourite" element={<Favourites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componenets/RecipItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipItems = () => {
+  return <div>RecipItems</div>;
+};
+
+export default RecipItems;
